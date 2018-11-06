859c658c659b4deb475c9d29163711281d9591d5
rasyidcode7 <rasyidcode7@gmail.com>
2018-11-07 03:05:16 +0700

    initial commit and add calc app

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textOperand1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textOperand2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Operand 1";
+            // 
+            // textOperand1
+            // 
+            this.textOperand1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.textOperand1.Location = new System.Drawing.Point(75, 19);
+            this.textOperand1.Name = "textOperand1";
+            this.textOperand1.Size = new System.Drawing.Size(69, 20);
+            this.textOperand1.TabIndex = 1;
+            this.textOperand1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textOperand1_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Operand 2";
+            // 
+            // textOperand2
+            // 
+            this.textOperand2.Location = new System.Drawing.Point(75, 52);
+            this.textOperand2.Name = "textOperand2";
+            this.textOperand2.Size = new System.Drawing.Size(69, 20);
+            this.textOperand2.TabIndex = 1;
+            this.textOperand2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textOperand2_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GrayText;
+            this.button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(10, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(31, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.add);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.GrayText;
+            this.button3.ForeColor = System.Drawing.SystemColors.Control;
+            this.button3.Location = new System.Drawing.Point(84, 94);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(31, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "/";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.divide);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.GrayText;
+            this.button4.ForeColor = System.Drawing.SystemColors.Control;
+            this.button4.Location = new System.Drawing.Point(47, 94);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(31, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "-";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.substract);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.GrayText;
+            this.button5.ForeColor = System.Drawing.SystemColors.Control;
+            this.button5.Location = new System.Drawing.Point(121, 94);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(31, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "x";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.multiply);
+            // 
+            // textResult
+            // 
+            this.textResult.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textResult.Location = new System.Drawing.Point(158, 19);
+            this.textResult.Multiline = true;
+            this.textResult.Name = "textResult";
+            this.textResult.ReadOnly = true;
+            this.textResult.Size = new System.Drawing.Size(45, 98);
+            this.textResult.TabIndex = 8;
+            this.textResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(210, 130);
+            this.Controls.Add(this.textResult);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textOperand2);
+            this.Controls.Add(this.textOperand1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textOperand1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textOperand2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textResult;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator {
+    public partial class Form1 : Form {
+        double operand1;
+        double operand2;
+        double result;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void add(object sender, EventArgs e) {
+            operand1 = Convert.ToDouble(textOperand1.Text);
+            operand2 = Convert.ToDouble(textOperand2.Text);
+
+            result = operand1 + operand2;
+            textResult.Text = result.ToString();
+        }
+
+        private void substract(object sender, EventArgs e)
+        {
+            operand1 = Convert.ToDouble(textOperand1.Text);
+            operand2 = Convert.ToDouble(textOperand2.Text);
+
+            result = operand1 - operand2;
+            textResult.Text = result.ToString();
+        }
+
+        private void divide(object sender, EventArgs e)
+        {
+            operand1 = Convert.ToDouble(textOperand1.Text);
+            operand2 = Convert.ToDouble(textOperand2.Text);
+
+            result = operand1 / operand2;
+            textResult.Text = result.ToString();
+        }
+
+        private void multiply(object sender, EventArgs e)
+        {
+            operand1 = Convert.ToDouble(textOperand1.Text);
+            operand2 = Convert.ToDouble(textOperand2.Text);
+
+            result = operand1 * operand2;
+            textResult.Text = result.ToString();
+        }
+
+        private void textOperand1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) == false && char.IsControl(e.KeyChar) == false && (e.KeyChar == '.' ? textOperand1.Text.Contains('.') == true : true))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textOperand2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) == false && char.IsControl(e.KeyChar) == false && (e.KeyChar == '.' ? textOperand2.Text.Contains('.') == true : true))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
